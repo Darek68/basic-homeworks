447adbd60f54639cdd05ca1eccd03efbe20bcb74
Darek <darek68@list.ru>
2023-11-02 09:56:59 +0300

    second commit hw7

--- src/main/java/ru/darek/homework10/Box.java
+++ src/main/java/ru/darek/homework10/Box.java
@@ -3,12 +3,11 @@ package ru.darek.homework10;
 import com.sun.source.tree.IfTree;
 
 public class Box {
-    private int length;
-    private int width;
-    private int height;
+    private final int length;
+    private final int width;
+    private final int height;
     String color;
-    //private Boolean isEmpty;
-    private Boolean isOpen;
+    private boolean isOpen;
     private String item;
 
     public Box(int length, int width, int height, String color) {
@@ -23,56 +22,62 @@ public class Box {
 
     @Override
     public String toString() {
-        String res="Коробка{длиной: " + length + " шириной: " + width + " высотой: " + height +
+        String res = "Коробка{длиной: " + length + " шириной: " + width + " высотой: " + height +
                 "\n цвета: " + color + "\n";
-        if (this.item==null){
-            res+=" пустая и ";
+        if (this.item == null) {
+            res += " пустая и ";
         } else {
-            res+=" заполенна предметом " + this.item + " и ";
+            res += " заполенна предметом " + this.item + " и ";
         }
-        if (isOpen){
-            res+=" открыта ";
+        if (isOpen) {
+            res += " открыта ";
         } else {
-            res+=" закрыта ";
+            res += " закрыта ";
         }
         return res;
     }
-    public void openBox(){
-        if (this.isOpen){
+
+    public void open() {
+        if (this.isOpen) {
             System.out.println("Коробка и так открыта!");
+            return;
         }
-        this.isOpen=true;
+        this.isOpen = true;
         System.out.println("Мы открыли коробку");
     }
-    public void closeBox(){
-        if (!this.isOpen){
+
+    public void close() {
+        if (!this.isOpen) {
             System.out.println("Коробка и так закрыта!");
+            return;
         }
-        this.isOpen=false;
+        this.isOpen = false;
         System.out.println("Мы закрыли коробку");
     }
-    public void putItem(String item){
-        if (!this.isOpen){
+
+    public void putItem(String item) {
+        if (!this.isOpen) {
             System.out.println("Нельзя положить в закрыту коробку!");
-        } else if (this.item!=null) {
+        } else if (this.item != null) {
             System.out.println("Нельзя положить в заполненую коробку!");
         } else {
-            this.item=item;
+            this.item = item;
             System.out.println("В коробку положили " + this.item);
         }
     }
-    public String getItem(){
-        String res=null;
-        if (!this.isOpen){
+
+    public String getItem() {
+        if (!this.isOpen) {
             System.out.println("Нельзя достать предмет из закрытой коробки!");
-        } else if (this.item==null){
-            System.out.println("Коробка пуста!");
-        } else {
-            System.out.println("Из коробки достали " + this.item);
-            res=this.item;
-            this.item=null;
-            return res;
+            return null;
         }
-        return item;
+        if (this.item == null) {
+            System.out.println("Коробка пуста!");
+            return null;
+        }
+        System.out.println("Из коробки достали " + this.item);
+        String res = this.item;
+        this.item = null;
+        return res;
     }
 }
--- src/main/java/ru/darek/homework10/MyApp.java
+++ src/main/java/ru/darek/homework10/MyApp.java
@@ -2,26 +2,26 @@ package ru.darek.homework10;
 
 public class MyApp {
     public static void main(String[] args) {
-        Box box=new Box(10,23,13,"серая");
+        Box box = new Box(10, 23, 13, "серая");
         System.out.println(box.toString());
-        box.color="черная";
+        box.color = "черная";
         System.out.println(box.toString());
         System.out.println();
         box.putItem("Предмет1");
         System.out.println();
-        box.openBox();
+        box.open();
         System.out.println(box.toString());
         System.out.println();
         box.putItem("Предмет1");
         System.out.println(box.toString());
         System.out.println();
         box.putItem("Предмет2");
-        box.closeBox();
+        box.close();
         System.out.println();
         box.getItem();
         System.out.println(box.toString());
         System.out.println();
-        box.openBox();
+        box.open();
         box.getItem();
         box.putItem("Предмет2");
         System.out.println(box.toString());
@@ -30,20 +30,20 @@ public class MyApp {
 
         System.out.println("-----------------------");
 
-        User[] users={new User("Сидоров","Игорь","Петрович",getYearOfBirth(),"sidorip@mail.ru"),
-                new User("Иванов","Денис","Сергеевич",getYearOfBirth(),"ivands@gmail.com"),
-                new User("Петров","Александ","Александрович",getYearOfBirth(),"alek123@list.ru"),
-                new User("Петров","Сергей","Александрович",getYearOfBirth(),"sery34@list.ru"),
-                new User("Петров","Михаил","Александрович",getYearOfBirth(),"hero777@mail.ru"),
-                new User("Петров","Данил","Александрович",getYearOfBirth(),"okden@list.ru"),
-                new User("Петрова","Надежда","Александровна",getYearOfBirth(),"nperova@ya.ru"),
-                new User("Попова","Екатерина","Павловна",getYearOfBirth(),"epavlovna@ya.ru"),
-                new User("Земягин","Полиграф","Полиграфович",getYearOfBirth(),"bigbos@gmail.org"),
-                new User("Шарикова","Занаида","Олеговна",getYearOfBirth(),"sharizo@umlf.de")
+        User[] users = {new User("Сидоров", "Игорь", "Петрович", getYearOfBirth(), "sidorip@mail.ru"),
+                new User("Иванов", "Денис", "Сергеевич", getYearOfBirth(), "ivands@gmail.com"),
+                new User("Петров", "Александ", "Александрович", getYearOfBirth(), "alek123@list.ru"),
+                new User("Петров", "Сергей", "Александрович", getYearOfBirth(), "sery34@list.ru"),
+                new User("Петров", "Михаил", "Александрович", getYearOfBirth(), "hero777@mail.ru"),
+                new User("Петров", "Данил", "Александрович", getYearOfBirth(), "okden@list.ru"),
+                new User("Петрова", "Надежда", "Александровна", getYearOfBirth(), "nperova@ya.ru"),
+                new User("Попова", "Екатерина", "Павловна", getYearOfBirth(), "epavlovna@ya.ru"),
+                new User("Земягин", "Полиграф", "Полиграфович", getYearOfBirth(), "bigbos@gmail.org"),
+                new User("Шарикова", "Занаида", "Олеговна", getYearOfBirth(), "sharizo@umlf.de")
         };
 
-        for (User user:users) {
-            if (2003-user.yearOfBirth>40){
+        for (User user : users) {
+            if (2003 - user.getYearOfBirth() > 40) {
                 System.out.println(user.toString());
                 System.out.println();
             }
@@ -51,7 +51,7 @@ public class MyApp {
     }
 
 
-    public static int getYearOfBirth(){
-        return (int)(Math.random()*100) + 1923;
+    public static int getYearOfBirth() {
+        return (int) (Math.random() * 100) + 1923;
     }
 }
