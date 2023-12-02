6a4e4cd10f09cd302bdaf1ae8a2b4c7189f2cdd3
Darek <darek68@list.ru>
2023-12-02 17:02:35 +0300

    third commit hw17

--- src/main/java/ru/darek/PhoneBook.java
+++ src/main/java/ru/darek/PhoneBook.java
@@ -1,12 +1,9 @@
 package ru.darek;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
-    private Map<String,List<String>> nameMap=new HashMap<String,List<String>>(); //для поиска по ФИО
+    private Map<String, HashSet<String>> nameMap=new HashMap<String,HashSet<String>>(); //для поиска по ФИО
     private Map<String,String> phoneMap=new HashMap<String,String>(); //для поиска по телефону
     public PhoneBook() {
     }
@@ -15,26 +12,22 @@ public class PhoneBook {
         if (phoneMap.containsKey(phone)) return ("Номер " + phone + " уже закреплен за абонентом: " + phoneMap.get(phone));
 
         //справочник ФИО
-        if (! nameMap.containsKey(name)) nameMap.put(name, new ArrayList<String>());
-        List<String> phones = nameMap.get(name);
+        if (! nameMap.containsKey(name)) nameMap.put(name, new HashSet<String>());
+        Set<String> phones = nameMap.get(name);
         phones.add(phone);
-        nameMap.put(name,phones);
 
         //справочник номеров
         phoneMap.put(phone,name);
 
         return "В книгу добалено: " + name + " с номером " +  phone;
     }
-    public String find(String name){
+    public HashSet<String> find(String name){
         if (nameMap.containsKey(name)){
-            return nameMap.get(name).toString();
+            return nameMap.get(name);
         }
         return null;
     }
     public boolean containsPhoneNumber(String phone){
-        if (phoneMap.containsKey(phone)){
-            return true;
-        }
-        return false;
+        return (phoneMap.containsKey(phone));
     }
 }
